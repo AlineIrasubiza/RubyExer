12ab69d0dd95e09c2eeef1f8190ec73f958261c2
Luc <luc.bayo@gmail.com>
2018-03-06 09:44:13 +0200

    modified file names

---
+++ 5-introToMethod.rb
@@ -0,0 +1,18 @@
+#Exercises
+
+#Use a method that changes the following string into all lowercase:
+
+    "YUrI TOMoko"
+
+#What do you expect if you run the method “reverse” on
+
+    "spaceship"
+    "racecar"
+    1776
+
+#Test your theory and try it yourself.
+#Fix the code to get the desired output - but this time do not add or eliminate any quotation marks:
+
+    puts 12 * "4"					# should produce => 48
+    puts 3 + " times a day"			# should produce => 3 times a day
+
---
+++ 6-variable.rb
@@ -0,0 +1,36 @@
+Exercises
+
+#Predict what happens when you run the following code:
+
+    a = 1
+    b = 2
+    c = a + b
+    d = c + a + b
+    puts d
+
+#Run the following code - you should see an error message. Fix the bug.
+
+    hello = "goodnight"
+    world = " moon"
+    puts hello + world
+    hello = cat
+    world = "fish"
+    puts hello + world
+    x = 1
+    y = 2
+    puts x + y
+
+#What does the following code do:
+
+    a = 1
+    b = a
+    puts b
+
+#What does the following code do:
+
+    a = 1
+    b = a
+    a = 2
+    puts b
+
+#Why do you think that is?
